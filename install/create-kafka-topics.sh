9be9a035aac22348cf9fd1eee0d6d184df38e85b
Reinaldy Rafli <aldy505@proton.me>
2024-02-04 12:59:03 +0700

    fix: kafka topic creation should use docker compose exec instead of run

--- install/create-kafka-topics.sh
+++ install/create-kafka-topics.sh
@@ -1,7 +1,17 @@
 echo "${_group}Creating additional Kafka topics ..."
 
-$dcr kafka 'cluster config set log_retention_ms 86400000'
-$dcr kafka 'cluster config set kafka_batch_max_bytes 52428800'
-$dcr kafka '-X brokers=kafka:9092 topic create ingest-attachments ingest-transactions ingest-events ingest-replay-recordings profiles ingest-occurrences ingest-metrics ingest-performance-metrics ingest-monitors'
+$dc up -d kafka
+$dc exec kafka rpk cluster config set log_retention_ms 86400000
+$dc exec kafka rpk cluster config set kafka_batch_max_bytes 52428800
+
+EXISTING_KAFKA_TOPICS=$($dc exec kafka rpk -X brokers=kafka:9092 topic list)
+NEEDED_KAFKA_TOPICS="ingest-attachments ingest-transactions ingest-events ingest-replay-recordings profiles ingest-occurrences ingest-metrics ingest-performance-metrics ingest-monitors"
+
+for topic in $NEEDED_KAFKA_TOPICS; do
+  if ! echo "$EXISTING_KAFKA_TOPICS" | grep -qE "(^)$topic"; then
+    $dc exec kafka rpk -X brokers=kafka:9092 topic create $topic
+    echo ""
+  fi
+done
 
 echo "${_endgroup}"
