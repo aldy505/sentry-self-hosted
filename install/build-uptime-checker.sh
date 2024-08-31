ae769deef3d82b5d3f97e8c385552e14caf57395
Reinaldy Rafli <aldy505@proton.me>
2024-08-31 07:26:07 +0700

    fix: differentiating uptime-checker config and git repo

--- install/build-uptime-checker.sh
+++ install/build-uptime-checker.sh
@@ -1,9 +1,11 @@
 echo "${_group}Building uptime-checker ... "
 
-rm -rf uptime-checker
+rm -rf uptime-checker-git
 
-git clone --depth 1 https://github.com/getsentry/uptime-checker.git
+git clone --depth 1 https://github.com/getsentry/uptime-checker.git uptime-checker-git
 
 $dcb --force-rm uptime-checker
+
+rm -rf uptime-checker-git
 
 echo "${_endgroup}"
